1f8eba120c44b78a9b7ee59b8c7db9faa99da4e4
artem4574 <115219395+artem4574@users.noreply.github.com>
2023-12-05 10:05:19 +0300

    fix first delta % 1 != 0

--- elliptic curve calculation.py
+++ elliptic curve calculation.py
@@ -126,12 +126,12 @@ def composition(point, k, a, p):
             x_r = (delta ** 2 - x_p - x_q) % p
             y_r = (((delta * (x_p - x_r)) % p) - y_p) % p
             ans_point = (int(x_r), int(y_r))
- 
+            print(delta, ans_point)
     if type(ans_point) is list:
         print("Composition = [", k, "]", "(", point[0], ",", point[1], ")", " = (", ans_point[0], ",", ans_point[1], ")", sep = '' )
     else: 
-        print("Composition = [", k, "]G = ", ans_point , sep = '' )
+        print("Composition = [", k, "]", "(", point[0], ",", point[1], ")", " = ", ans_point, sep = '' )
 
-composition([0,10], 4, 1, 11)
+composition([0,10], 5, 1, 11)
 
 
