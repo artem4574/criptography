acf044ca93a10d225a067a71d024f9668159957a
artem4574 <115219395+artem4574@users.noreply.github.com>
2024-03-07 00:10:58 +0300

    block_K upd

--- block_K_Diffie–Hellman Key Exchange.py
+++ block_K_Diffie–Hellman Key Exchange.py
@@ -1,21 +1,30 @@
-shared_n = int(input("Enter shared n:"))
-shared_a = int(input("Enter shared a:"))
+import random
 
-secret_1 = int(input("Enter secret_1:"))
-secret_2 = int(input("Enter secret_2:"))
+shared_n = int(input("Enter shared n (prime): "))
+shared_a = int(input("Enter shared a: "))
+if shared_a > shared_n:
+    print("A > n")
+    exit()
+if shared_a <= 1 or shared_n <= 1:
+    print("N and A should be > 1!")
+
+secret_1: int = random.randint(2, shared_n - 1)
+secret_2: int = random.randint(2, shared_n - 1)
 
 print("Publicly Shared Variables:")
-print("  Publicly shared n: ", shared_n)
+print("\n  Publicly shared n: ", shared_n)
 print("  Publicly shared a: ", shared_a)
 
 Y_a = (shared_a ** secret_1) % shared_n
-print("\n  Ya sended Over Public Chanel: ", Y_a)
-
 Y_b = (shared_a ** secret_2) % shared_n
+
+print("\n  Ya sended Over Public Chanel: ", Y_a)
 print("  Yb sended Over Public Chanel: ", Y_b)
 
-print(" \n Privately Calculated Shared Secret:")
+if Y_a == 1 or Y_b == 1:
+    print("Open key = 1!")
+    exit()
 
+print(" \nPrivately Calculated Shared Secret:")
 print("  Ka: ", (Y_b ** secret_1) % shared_n)
-
 print("  Kb: ", (Y_a**secret_2) % shared_n)
